0439e377455532cec7b632792eb3f601e585813b
yusuf <mu.yusuf.99@gmail.com>
2021-10-02 15:31:30 +0300

    Initial Commit

---
+++ NEWS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NEWS
+//
+//  Created by Yusuf Şimşek on 2.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NEWS/NEWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NEWSApp.swift
+//  NEWS
+//
+//  Created by Yusuf Şimşek on 2.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct NEWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
